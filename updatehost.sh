a7766f5edf478c8072e6d406a27709793a49697d
root <root@network.corp.ncodeit.com>
2018-06-26 13:28:43 +0530

    etc/hosts entry fixed

--- updatehost.sh
+++ updatehost.sh
@@ -6,7 +6,7 @@ hostnamectl set-hostname ${myncdid}-$hhmm.corp.ncodeit.com
 echo "Hostname Updated to $(hostnamectl)"
 echo "populating the /etc/hosts file with current hostname and ip"
 read -p "Enter the IP address of your server: " myip
-echo "$myip ${myncdid}.corp.ncodeit.com" >> /etc/hosts 
+echo "$myip ${myncdid}-$hhmm.corp.ncodeit.com" >> /etc/hosts 
 echo "Your updated /etc/hosts file is "
 cat /etc/hosts
 echo "run the script again if errors found in the info provided"
