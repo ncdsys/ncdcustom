a78671b62ef13999ca2365766833279d5ebe8c2d
root <root@network.corp.ncodeit.com>
2018-06-26 12:24:50 +0530

    modified to include ncodeitadm user profile

--- ncdcustomize.sh
+++ ncdcustomize.sh
@@ -2,10 +2,15 @@
 sudo yum -y install epel-release
 sudo yum -y install figlet
 sudo echo 'export PS1="\[\e[1;32m[\u@\h \W]\$ \e[m\]"' >> $HOME/.bash_profile
+sudo echo 'export PS1="\[\e[1;32m[\u@\h \W]\$ \e[m\]"' >> /home/ncodeitadm/.bash_profile
 sudo echo 'figlet -ctk "nCodeIT . com"' >> $HOME/.bash_profile
+sudo echo 'figlet -ctk "nCodeIT . com"' >> /home/ncodeitadm/.bash_profile
 IP="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
 HOSTNAME=$(hostname)
 NOW=$(date +"%e %b %Y, %a %r")
 sudo echo 'echo "You have logged in from '$IP' into '$HOSTNAME' at '$NOW'."'  >> $HOME/.bash_profile
+sudo echo 'echo "You have logged in from '$IP' into '$HOSTNAME' at '$NOW'."'  >> /home/ncodeitadm/.bash_profile
 
 sudo echo 'echo "You have logged into nCodeIT Enterprise Infrastructure. All activities are monitored and logged on this system for security purposes"' >> $HOME/.bash_profile
+
+sudo echo 'echo "You have logged into nCodeIT Enterprise Infrastructure. All activities are monitored and logged on this system for security purposes"' >> /home/ncodeitadm/.bash_profile
