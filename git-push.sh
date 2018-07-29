6f13fcf586b34590f46cacdc555d6902f5be390e
ncodeit <ncodeit@orabox2.corp.ncodeit.com>
2018-07-29 10:48:44 +0530

    file added

---
+++ git-push.sh
@@ -0,0 +1,7 @@
+#execute this script only after cloning the remote repo first
+#without clone the remote repo first, the origin and master will not be seet
+# git startus  # shows all the tracked,untracked files
+# git add <filename>  # adds the specified file to the local git
+# git commit -m "comment"   # commits the added file to the local repo
+
+git push origin master
