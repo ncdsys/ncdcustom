5ad10f2ca2f382d210616bcb9ff6213193dab2c1
root <root@ncdpeppercone142.corp.ncodeit.com>
2018-09-26 19:36:26 +0530

    fix usremod error

--- createadminuser.sh
+++ createadminuser.sh
@@ -3,7 +3,7 @@ useradd -m -d /home/ncodeitadm ncodeitadm
 echo "setup password for ncodeitadm user. use default ncodeit123"
 passwd ncodeitadm
 #usermod -aG wheel ncodeitadm
-usermod -aG kvm libvirt ncodeitadm
+usermod -aG kvm,libvirt ncodeitadm
 echo "ncodeitadm user creaeted successfully"
 echo "This user has superuser previleges and all the admin activities should be performed under this user only or using sudo"
 echo "DO NOT USE ROOT USER ANY MORE. Use ncodeitadm "
